c18963b68fc126d7391246dd158916ba9b84753c
evelynalvees <evelyn.alvees0312@gmail.com>
2025-08-01 17:02:17 -0300

    Add files via upload

---
+++ MyClass (1).java
@@ -0,0 +1,23 @@
+
+
+public class MyClass {
+  public static void main(String args[]) {
+      //1 - Cálculo para empresa 
+double serviço1 = 300.00; // número quebrado
+double serviço2 = 150.00; // número quebrado
+double serviço3 = 499.99; // número quebrado
+double pagar = serviço1+ serviço2 + serviço3;
+System.out.println ("cliente deverá pagar: " + pagar );
+//2 - Cálculo de salário 
+double recebe = 35.00;
+int horas = 8;
+double receber = recebe * horas;
+System.out.println (" No final de um dia ele deve receber:" + receber);
+// 3- final de um dia ele deve receber
+int caixa = 27;
+int comprou = 16;
+int tem = caixa * comprou;
+System.out.println ("Pamela terá quantos bomnons? " + tem)
+
+}
+}
